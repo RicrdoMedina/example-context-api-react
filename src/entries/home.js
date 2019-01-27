99e0f42aafa4200b0751c1d48aa2c1e689905adb
Ricardo Medina <ricardomedina@localhost.localdomain>
2019-01-27 12:14:37 -0400

    Componentes y Context Provider anidados

--- src/entries/home.js
+++ src/entries/home.js
@@ -7,6 +7,19 @@ const ThemeContext = React.createContext('light')
 
 class ThemeProvider extends React.Component {
   /* code */
+  state = {theme: 'light'}
+  toggleTheme = () => {
+    this.setState(({theme}) => ({
+      theme: theme === 'light' ? 'dark' : 'light',
+    }))
+  }
+  render() {
+    return (
+      <ThemeContext.Provider value={this.state.theme}>
+        {this.props.children}
+      </ThemeContext.Provider>
+    )
+  }
 }
 
 const ThemeConsumer = ThemeContext.Consumer
@@ -15,6 +28,17 @@ const LanguageContext = React.createContext('en')
 
 class LanguageProvider extends React.Component {
   /* code */
+  state = {lang: 'en'}
+  setLanguage = lang => {
+    this.setState({lang})
+  }
+  render() {
+    return (
+      <LanguageContext.Provider value={this.state.lang}>
+        {this.props.children}
+      </LanguageContext.Provider>
+    )
+  }
 
 }
 
