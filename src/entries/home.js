1f113c51b0fc096a1bc22d9cfa2c6fc150dbe366
Ricardo Medina <ricardomedina@localhost.localdomain>
2019-01-27 11:10:58 -0400

    Agregando estructura de componentes anidados

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,10 +1,62 @@
 import React from 'react'
 import { render } from 'react-dom'
 //import Red from '../pages/containers/Red.js'
-import Grandmother from '../pages/containers/StoreFamily'
+//import Grandmother from '../pages/containers/StoreFamily'
+
+const ThemeContext = React.createContext('light')
+
+class ThemeProvider extends React.Component {
+  /* code */
+}
+
+const ThemeConsumer = ThemeContext.Consumer
+
+const LanguageContext = React.createContext('en')
+
+class LanguageProvider extends React.Component {
+  /* code */
+
+}
+
+const LanguageConsumer = LanguageContext.Consumer
+
+function AppProviders({children}) {
+  return (
+    <LanguageProvider>
+      <ThemeProvider>
+        {children}
+      </ThemeProvider>
+    </LanguageProvider>
+  )
+}
+
+function ThemeAndLanguageConsumer({children}) {
+  return (
+    <LanguageConsumer>
+      {language => (
+        <ThemeConsumer>
+          {theme => children({language, theme})}
+        </ThemeConsumer>
+      )}
+    </LanguageConsumer>
+  )
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <AppProviders>
+        <ThemeAndLanguageConsumer>
+          {({theme, language}) => <div>{theme} and {language}</div>}
+        </ThemeAndLanguageConsumer>
+      </AppProviders>
+    )
+  }
+}
 
 const app = document.getElementById('app')
+
 render(
-        <Grandmother/>,
+        <App/>,
         app
       )
